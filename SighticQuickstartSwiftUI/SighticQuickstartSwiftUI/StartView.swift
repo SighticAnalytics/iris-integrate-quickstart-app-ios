351d1a53f9daa32b5cf9e1ff554cfd4525f44305
GitHub <marcus.hedenstrom@eyescanner.se>
2022-11-11 16:39:35 +0100

    Check SDK version and iDevice supported.

--- SighticQuickstartSwiftUI/SighticQuickstartSwiftUI/StartView.swift
+++ SighticQuickstartSwiftUI/SighticQuickstartSwiftUI/StartView.swift
@@ -5,8 +5,15 @@ import SighticAnalytics
 
 struct StartView: View {
     @Binding var appState: AppState
-
+    
+    @State private var unsupportedSdkVersion = false
+    @State private var runAnyway = false
+    
     var body: some View {
+        let showWarning =
+            unsupportedSdkVersion ||
+            !SighticSupportedDevices.isCurrentDeviceSupported
+        
         VStack {
             Text("Sightic SDK Quickstart")
                 .font(.title)
@@ -18,9 +25,49 @@ struct StartView: View {
             Text("StartView")
                 .font(.title2)
                 .padding()
-            Button(action: { appState = .test },
-                   label: { Text("Go to test") })
+            if !SighticSupportedDevices.isCurrentDeviceSupported {
+                Text("Unsupported iDevice")
+                    .foregroundColor(.red)
+            }
+            if unsupportedSdkVersion {
+                Text("Unsupported SDK version (\(SighticVersion.sdkVersion))")
+                   .foregroundColor(.red)
+            }
+            Button(showWarning ? "Go to test anyway" : "Go to test") { goToTest() }
+                .padding()
             Spacer()
+        }
+    }
+    
+    func goToTest() {
+        Task {
+            await checkSdkVersions()
+            
+            if unsupportedSdkVersion {
+                guard runAnyway else {
+                    // Make user click the button again
+                    runAnyway = true
+                    return
+                }
+            }
+
+            appState = .test
+        }
+    }
+    
+    func checkSdkVersions() async {
+        switch await SighticVersion.sdkVersions(apiKey: "") {
+        case let .failure(error):
+            print("Error while checking for supported versions: \(error)")
+        case let .success(versions):
+            print(versions)
+            if !versions.isCurrentVersionSupported {
+                print("Current version is not supported. Supported versions are: \(versions.supportedVersions)")
+                unsupportedSdkVersion = true
+            }
+            else {
+                unsupportedSdkVersion = false
+            }
         }
     }
 }
